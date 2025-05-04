ecaad53ec3576052291d3feec7cee6db9d5c1d9e
edahigure <edahigure@gmail.com>
2025-05-04 09:12:07 -0600

    Fix bootstrap instalation

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,6 @@
+//= link_tree ../images
+//= link_tree ../builds  # Required if using Turbo/Stimulus with importmaps
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+//= link application.css
+//= link application.js
